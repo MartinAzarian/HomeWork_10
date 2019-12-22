95fe0a34014803c17709388502b80039220bb660
MartsLine <48711841+MartsLine@users.noreply.github.com>
2019-12-22 18:49:17 +0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
+import com.company.converter.*;
+import com.company.currency.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Converter dollarConverter = new DollarConverter();
+
+        Dollar dollar = new Dollar();
+
+        Euro euro = new Euro();
+        PoundSterling poundSterling = new PoundSterling();
+        Ruble ruble = new Ruble();
     }
 }
